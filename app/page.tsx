ce8db053b292d38b31c2bc5c4886d0e1a3fb8f16
Angela Wen <angelawen526@gmail.com>
2024-02-13 23:09:44 +1100

    setting up repo and landing page

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+import { AspectRatio, Box, Stack, Typography } from '@mui/joy';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <Stack>
+        <AspectRatio sx={{height: "calc(100vh - 75px)", width: "100%", overflow:"hidden"}} objectFit="cover">
+          <Image src={"/group_photo.JPG"} alt={"group of people"} layout="fill"/>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              bgcolor: "#93AED3",
+              opacity: "0.6",
+            }}
+          />
+          <Stack
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            alignItems="center"
+            justifyContent="center"
+            spacing={8}
+          >
+            <Typography component="h1" fontSize="70pt" sx={{color:"#FFFFFF"}}>
+              UNSW Women In Engineering
+            </Typography>
+            <Typography component="h2" fontSize="30pt" sx={{color:"#FFFFFF"}}>
+              Connect • Engage • Inspire
+            </Typography>
+          </Stack>
+
+        </AspectRatio>
+        <Stack>
+
+        </Stack>
+    </Stack>
+  );
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,45 @@
+import { Sheet, Stack, Typography } from '@mui/joy';
+import Image from 'next/image';
+import Link from 'next/link';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import InstagramIcon from '@mui/icons-material/Instagram';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
+export default function NavBar() {
+  return (
+    <Sheet sx={{height: 75}}>
+      <Stack width="100%" height="100%" boxShadow="md" direction="row" alignItems="center" pl={2}>
+        <Link href="/">
+          <Image src={"/wiesoc-logo-navbar.svg"} alt={"wiesoc-logo"} width={300} height={75}/>
+        </Link>
+        <Stack direction="row" spacing={8} ml={5}>
+          <Typography level="title-lg" component={Link} href="/about-us" sx={{textDecoration: "none"}}>
+            About Us
+          </Typography>
+          <Typography level="title-lg" component={Link} href="/events" sx={{textDecoration: "none"}}>
+            Events
+          </Typography>
+          <Typography level="title-lg" component={Link} href="/programs" sx={{textDecoration: "none"}}>
+            Programs
+          </Typography>
+          <Typography level="title-lg" component={Link} href="/sponsors" sx={{textDecoration: "none"}}>
+            Sponsors
+          </Typography>
+          <Typography level="title-lg" component={Link} href="/jobs-board" sx={{textDecoration: "none"}}>
+            Jobs Board
+          </Typography>
+          <Typography level="title-lg" component={Link} href="/contact-us" sx={{textDecoration: "none"}}>
+            Contact Us
+          </Typography>
+
+        </Stack>
+        <Stack direction="row" >
+          {/*<FacebookIcon/>*/}
+          {/*<InstagramIcon/>*/}
+          {/*<LinkedInIcon/>*/}
+        </Stack>
+
+
+      </Stack>
+    </Sheet>
+  )
+}
