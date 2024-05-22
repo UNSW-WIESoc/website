4e2facd19872ae20a0af3cc85f6f3b97ae607dec
Angela Wen <angela@angelas-air.lan>
2024-05-22 23:37:24 +1000

    added a page header image title and added to the programs page

--- app/(pages)/programs/page.tsx
+++ app/(pages)/programs/page.tsx
@@ -1,8 +1,10 @@
 import { Stack } from '@mui/joy';
+import PageHeaderCard from "@/components/PageHeaderCard";
 
 export default function Program() {
   return (
     <Stack>
+      <PageHeaderCard imagePath={'/2024_group_photo.JPG'} pageTitle={'Programs'}/>
       PROGRAMS
     </Stack>
   );
