63a1d93aee53801c97d48aec3c6306a4bb166e56
sophiac6410 <79835146+sophiac6410@users.noreply.github.com>
2024-05-23 10:14:55 +1000

    fixed text sizes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 function CoverImage() {
   return (
-    <AspectRatio sx={{width: "100%", overflow:"hidden"}} objectFit="cover">
-    <Image src={"/2024_group_photo.JPG"} alt={"group of people"} fill/>
+    <AspectRatio sx={{width: '100%', overflow:'hidden'}} objectFit='cover'>
+    <Image src={'/2024_group_photo.JPG'} alt={'group of people'} fill/>
     <Box
       sx={{
         position: 'absolute',
@@ -13,8 +13,8 @@ function CoverImage() {
         left: 0,
         width: '100%',
         height: '100%',
-        bgcolor: "#93AED3",
-        opacity: "0.7",
+        bgcolor: '#93AED3',
+        opacity: '0.7',
       }}
     />
     <Stack
@@ -25,14 +25,14 @@ function CoverImage() {
         width: '100%',
         height: '100%',
       }}
-      alignItems="center"
-      justifyContent="center"
+      alignItems='center'
+      justifyContent='center'
       spacing={8}
     >
-      <Typography level="h3" fontSize="72px" sx={{color:"#FFFFFF", textAlign: 'center'}}>
+      <Typography level='h3' fontSize='72px' sx={{color:'#FFFFFF', textAlign: 'center'}}>
         UNSW Women In Engineering
       </Typography>
-      <Typography level="title-lg" fontSize="30px" sx={{color:"#FFFFFF"}}>
+      <Typography level='title-lg' fontSize='30px' sx={{color:'#FFFFFF'}}>
         Connect • Engage • Inspire
       </Typography>
     </Stack>
@@ -41,35 +41,35 @@ function CoverImage() {
 }
 
 function AboutUsOverview() {
-  const aboutUsText = "We are a UNSW student-run society that facilitates members from all disciplines of engineering. \n \
-  We run many social and industry events throughout the year as well as two Programs dedicated to mentoring and interpersonal development.\n"
+  const aboutUsText = 'We are a UNSW student-run society that facilitates members from all disciplines of engineering. \n \
+  We run many social and industry events throughout the year as well as two Programs dedicated to mentoring and interpersonal development.\n'
 
   return (
     <Box mx='auto' my={15} width='1000px' height='100%'>
-        <Typography level="h2" pb={3}>About Us</Typography>
+        <Typography level='h2' pb={3}>About Us</Typography>
         <Stack direction='row' spacing={8}>
-          <Box width="500px">
-            <Typography level="body-lg" pb={3}>{aboutUsText}</Typography>
-            <Stack direction="row" width="100%" height="100%">
-              <Box width="100%" textAlign='center'>
+          <Box width='500px'>
+            <Typography level='body-lg' pb={3}>{aboutUsText}</Typography>
+            <Stack direction='row' width='100%' height='100%'>
+              <Box width='100%' textAlign='center'>
                 <PersonIcon sx={{fontSize: '4rem', color: '#93AED3'}}/>
-                <Typography level="body-md" fontWeight="600" textColor="#063776">2000+</Typography>
-                <Typography level="body-md" fontWeight="600" textColor="#063776">members</Typography>
+                <Typography level='body-md' fontWeight='600' textColor='#063776'>2000+</Typography>
+                <Typography level='body-md' fontWeight='600' textColor='#063776'>members</Typography>
               </Box>
-              <Box width="100%" textAlign='center'>
+              <Box width='100%' textAlign='center'>
                 <PersonIcon sx={{fontSize: '4rem', color: '#93AED3'}}/>
-                <Typography level="body-md" fontWeight="600" textColor="#063776">2000+</Typography>
-                <Typography level="body-md" fontWeight="600" textColor="#063776">members</Typography>
+                <Typography level='body-md' fontWeight='600' textColor='#063776'>2000+</Typography>
+                <Typography level='body-md' fontWeight='600' textColor='#063776'>members</Typography>
               </Box>
-              <Box width="100%" textAlign='center'>
+              <Box width='100%' textAlign='center'>
                 <PersonIcon sx={{fontSize: '4rem', color: '#93AED3'}}/>
-                <Typography level="body-md" fontWeight="600" textColor="#063776">2000+</Typography>
-                <Typography level="body-md" fontWeight="600" textColor="#063776">members</Typography>
+                <Typography level='body-md' fontWeight='600' textColor='#063776'>2000+</Typography>
+                <Typography level='body-md' fontWeight='600' textColor='#063776'>members</Typography>
               </Box>
             </Stack>
           </Box>
           <Box width='500px'>
-            <iframe width="100%" height='100%' src="https://www.youtube.com/embed/Tyyl9Kp-yiE?si=kk2dfErd_cJNYBbh" title="YouTube video player" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe width='100%' height='100%' src='https://www.youtube.com/embed/Tyyl9Kp-yiE?si=kk2dfErd_cJNYBbh' title='YouTube video player' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerPolicy='strict-origin-when-cross-origin' allowFullScreen></iframe>
           </Box>
         </Stack>
       </Box>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,29 @@
-import { Box, Typography, Stack } from "@mui/joy";
+import { Box, Typography, Stack } from '@mui/joy';
 import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <Box sx={{backgroundColor:'#D1E2F8', padding: '20px'}}>
+    <Box sx={{backgroundColor:'#D1E2F8', padding: '40px'}}>
       <center>
-        <Typography>Join Us Here</Typography>
-        <Stack direction="row" justifyContent="center">
-          <Link href="/">
-            <Box sx={{padding: '40px'}}>
-              <Image src={"/icon_facebook.svg"} alt={"facebook-logo"} height={30} width={30}/>
-              <Typography>Facebook</Typography>
+        <Typography level='h3' sx={{padding: '20px'}}>Join Us Here</Typography>
+        <Stack direction='row' justifyContent='center'>
+          <Link href='/'>
+            <Box sx={{padding: '40px', textDecoration: 'none'}}>
+              <Image src={'/icon_facebook.svg'} alt={'facebook-logo'} height={30} width={30}/>
+              <Typography level='body-md' sx={{color: '#063776'}}>Facebook</Typography>
             </Box>
           </Link>
-          <Link href="/">
-            <Box sx={{padding: '40px'}}>
-              <Image src={"/icon_instagram.svg"} alt={"instagram-logo"} height={30} width={30}/>
-              <Typography>Instagram</Typography>
+          <Link href='/'>
+            <Box sx={{padding: '40px', textDecoration: 'none'}}>
+              <Image src={'/icon_instagram.svg'} alt={'instagram-logo'} height={30} width={30}/>
+              <Typography sx={{fontSize: '16pt', color: '#063776'}}>Instagram</Typography>
             </Box>
           </Link>
-          <Link href="/">
-            <Box sx={{padding: '40px'}}>
-              <Image src={"/icon_linkedin.svg"} alt={"linkedin-logo"} height={30} width={30}/>
-              <Typography>LinkedIn</Typography>
+          <Link href='/'>
+            <Box sx={{padding: '40px', textDecoration: 'none'}}>
+              <Image src={'/icon_linkedin.svg'} alt={'linkedin-logo'} height={30} width={30}/>
+              <Typography sx={{fontSize: '16pt', color: '#063776'}}>LinkedIn</Typography>
             </Box>
           </Link>
         </Stack>
