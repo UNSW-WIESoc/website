182e5fc825a37a69f32c18928071dc9801206ed3
Rebecca Hsu <rbeccahsu@gmail.com>
2024-06-03 00:44:39 +1000

    Hardcoded events card and events page

--- app/(pages)/events/page.tsx
+++ app/(pages)/events/page.tsx
@@ -1,13 +1,43 @@
 import EventsCard from '../../../components/EventsCard';
 
-import { Stack } from '@mui/joy';
+import { Stack, Grid, Box } from '@mui/joy';
 import PageHeaderCard from "@/components/PageHeaderCard";
+import { Typography } from '@mui/joy';
 
 export default function Events() {
   return (
     <Stack>
       <PageHeaderCard imagePath={'/events/header.jpg'} pageTitle={'Events'}/>
-      <EventsCard/>
+      <Typography level="h1" sx={{ mt: 10 }}>Upcoming Events</Typography>
+      <Typography level="body-md" sx={{ textAlign: 'center', mb: 10 }}>
+        WIESoc hold many events throughout the year, with a great mix of industry 
+        and social events. Come and join us for our upcoming events!
+      </Typography>
+      <Box
+        display="flex"
+        justifyContent="center"
+        sx={{ width: '100%', mb: 10 }}
+      >
+        <Grid 
+          container 
+          justifyContent="center"
+          sx={{ width: '70%' }}
+          spacing={5}
+        >
+          {Array.from(Array(6)).map((_, index) => (
+            <Grid 
+              item 
+              xs={12} sm={6} md={6} 
+              key={index}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <EventsCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Stack>
   );
 }
--- components/EventsCard.tsx
+++ components/EventsCard.tsx
@@ -6,19 +6,46 @@ import Divider from '@mui/joy/Divider';
 import Typography from '@mui/joy/Typography';
 import Image from 'next/image';
 import Button from '@mui/joy/Button';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { Box } from '@mui/joy';
 
+// Hardedcoded for now
+// TODO (rbeccahsu): Work on integrating website with Contentful
 
 export default function EventsCard() {
   return (
-    <Card variant="outlined" sx={{ width: 600 }}>
+    <Card 
+      variant="outlined" 
+      sx={{
+        width: 600,
+        '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' },
+      }}
+    >
       <CardOverflow>
-        <AspectRatio ratio="2">
+        <AspectRatio objectFit="contain">
           <Image src={"/event_banner.JPG"} alt={"group of people"} fill/>
         </AspectRatio>
       </CardOverflow>
       <CardContent>
-        <Typography level="title-lg">WIESoc Study Lounge</Typography>
+        <Typography level="h3" fontSize="xl" sx={{ mt: 1 }}>WIESoc Study Lounge</Typography>
         <Typography level="body-sm">Wednesday 17th April, 12:00 - 14:00</Typography>
+
+        <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+          <Button 
+            component="a" 
+            href="https://fb.me/e/6IzxSbHFH"
+            startDecorator={<FacebookIcon />}
+            sx={{ 
+              mt: 2, 
+              width: 1/3, 
+              bgcolor: '#9396D3', 
+              '&:hover': { backgroundColor: '#777AD5' },
+              mx: 'auto'
+            }}
+            >
+            View Event
+            </Button>
+        </Box>
       </CardContent>
     </Card>
   )
