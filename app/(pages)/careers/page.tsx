72aadcfc0ef24315a13b44fad1669665a5bc60f1
DaisyYye <daisydaisyye@gmail.com>
2024-06-06 23:17:12 +1000

    attempted careers page text, havent tested

--- app/(pages)/careers/page.tsx
+++ app/(pages)/careers/page.tsx
@@ -1,10 +1,25 @@
-import { Stack } from '@mui/joy';
+import {Box, Stack, Typography} from '@mui/joy';
 import PageHeaderCard from "@/components/PageHeaderCard";
 
 export default function Careers() {
-  return (
-    <Stack>
-      <PageHeaderCard imagePath={'/careers/header.jpg'} pageTitle={'Careers'}/>
-    </Stack>
+
+   return (
+<Box>
+      <Stack>
+        <PageHeaderCard imagePath={'/careers/header.jpg'} pageTitle={'Careers'}/>
+      </Stack>
+
+      <Box mt={2} textAlign="center">
+        <Typography level="h1" className="graphite">
+          Jobs and Opportunities
+        </Typography>
+
+        <Typography level="body" className="graphite">
+          Discover corporate sponsored events and activities to expand your professional networks and skills.
+        </Typography>
+        
+      </Box>
+    </Box>
   );
+
 }
