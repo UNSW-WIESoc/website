bceed22e57daeaf096c413c64caf1faa0dd0e31b
unknown <aurelia.elaine@gmail.com>
2024-06-16 22:01:34 +1000

    Added SponsorRows function to reduce repition and fixed mobile responsiveness

--- app/(pages)/sponsors/page.tsx
+++ app/(pages)/sponsors/page.tsx
@@ -2,40 +2,75 @@ import { Box, Stack, Typography } from '@mui/joy';
 import PageHeaderCard from '@/components/PageHeaderCard';
 import HeadingBodyText from '@/components/HeadingBodyText';
 import SponsorSection from '@/components/SponsorSection';
+import Image from 'next/image';
+
+interface sponsorRows {
+  maxWidth: string;
+  imageComponent: string;
+  imageName: string;
+}
+
+function SponsorRows ({maxWidth, imageComponent, imageName}: sponsorRows) {
+  return(
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        maxWidth: maxWidth
+      }}
+    >
+      <Image src={imageComponent} alt={imageName} layout='responsive' width={500} height={200}/>
+    </Box>
+  )
+}
 
 export default function Sponsors() {
   return (
     <Stack>
       <PageHeaderCard imagePath={'/sponsors/header.jpg'} pageTitle={'Sponsors'}/>
       <HeadingBodyText heading='Our Sponsors' body='WIESoc is proudly supported by the UNSW Faculty of Engineering and our industry partners.' color='graphite'/>
-      <Box display="flex" justifyContent="center" mt={2} mb={10}>
-        <img src="/sponsors/unsw_logo.jpg" alt="UNSW Engineering" width={500} height="100%"/>
+      <Box 
+        display='flex' 
+        justifyContent='center' 
+        mt={2} 
+        mb={10}
+        sx={{
+          width: {xs: '440px', sm: '475px', md: '500px', lg: '525px'},
+          mx: 'auto',
+          height: 'auto'
+        }}  
+      >
+        <Box 
+          sx={{ 
+            position: 'relative',
+            width: '100%',
+            paddingBottom: '20%',
+            }}
+          >
+          <Image src="/sponsors/unsw_logo.jpg" alt="UNSW Engineering" fill/>
+        </Box>
       </Box>
       <Box width='95%' mx='auto' mb={15}>
         <SponsorSection title='Premium Sponsors'/>
         <Stack direction="column" spacing={8} justifyContent="center" alignItems="center">
           <Stack 
-            direction="row" 
-            spacing={{ xs: 5, sm: 10, md: 15, lg: 20}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '500px' }}>
-              <img src="/sponsors/atlassian.svg" alt="ATLASSIAN" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '500px' }}>
-              <img src="/sponsors/janeStreet_logo.jpg" alt="Jane Street" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='400px' imageComponent='/sponsors/atlassian.svg' imageName='Atlassian'></SponsorRows>
+            <SponsorRows maxWidth='400px' imageComponent='/sponsors/janeStreet_logo.jpg' imageName='Jane Street'></SponsorRows>
           </Stack>
           <Stack
-            direction="row" 
-            spacing={{ xs: 5, sm: 10, md: 15, lg: 20}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '340px' }}>
-              <img src="/sponsors/sydneyWaters.jpg" alt="Sydney Waters" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='340px' imageComponent='/sponsors/sydneyWaters.jpg' imageName='Sydney Waters'></SponsorRows>
           </Stack>
         </Stack>
       </Box>
@@ -43,49 +78,37 @@ export default function Sponsors() {
         <SponsorSection title='Advanced Sponsors'/>
         <Stack direction="column" spacing={8} justifyContent="center" alignItems="center">
           <Stack 
-            direction="row" 
-            spacing={{ xs: 2, sm: 5, md: 10, lg: 15}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '320px' }}>
-              <img src="/sponsors/cochlear.jpg" alt="Cochlear" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '340px' }}>
-              <img src="/sponsors/aecom.jpg" alt="AECOM" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '340px' }}>
-              <img src="/sponsors/dolby.jpg" alt="Dolby" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='320px' imageComponent='/sponsors/cochlear.jpg' imageName='Cochlear'></SponsorRows>
+            <SponsorRows maxWidth='340px' imageComponent='/sponsors/aecom.jpg' imageName='Aecom'></SponsorRows>
+            <SponsorRows maxWidth='340px' imageComponent='/sponsors/dolby.jpg' imageName='Dolby'></SponsorRows>
+
           </Stack>
           <Stack 
-            direction="row" 
-            spacing={{ xs: 5, sm: 10, md: 15, lg: 20}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '400px' }}>
-              <img src="/sponsors/sportsGrid.jpg" alt="Sports Grid" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '450px' }}>
-              <img src="/sponsors/systa.jpg" alt="SYSTRA" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='400px' imageComponent='/sponsors/sportsGrid.jpg' imageName='Sports Grid'></SponsorRows>
+            <SponsorRows maxWidth='450px' imageComponent='/sponsors/systa.jpg' imageName='Systra'></SponsorRows>
           </Stack>
           <Stack 
-            direction="row" 
-            spacing={{ xs: 2, sm: 5, md: 10, lg: 15}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '300px' }}>
-              <img src="/sponsors/appian.svg" alt="Appian" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '400px' }}>
-              <img src="/sponsors/projectServices.jpg" alt="Project Services" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '450px' }}>
-              <img src="/sponsors/stantec.jpg" alt="Stantec" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='300px' imageComponent='/sponsors/appian.svg' imageName='Appian'></SponsorRows>
+            <SponsorRows maxWidth='400px' imageComponent='/sponsors/projectServices.jpg' imageName='Project Services'></SponsorRows>
+            <SponsorRows maxWidth='450px' imageComponent='/sponsors/stantec.jpg' imageName='Stantec'></SponsorRows>
           </Stack>
         </Stack>
       </Box>
@@ -93,27 +116,23 @@ export default function Sponsors() {
         <SponsorSection title='Foundation Sponsors'/>
         <Stack direction="column" spacing={8} justifyContent="center" alignItems="center">
           <Stack 
-            direction="row" 
-            spacing={{ xs: 5, sm: 10, md: 15, lg: 20}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '450px' }}>
-              <img src="/sponsors/honeywell.jpg" alt="Honeywell" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '400px' }}>
-              <img src="/sponsors/flowTraders.jpg" alt="Flow Traders" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='450px' imageComponent='/sponsors/honeywell.jpg' imageName='Honeywell'></SponsorRows>
+            <SponsorRows maxWidth='400px' imageComponent='/sponsors/flowTraders.jpg' imageName='Flow Traders'></SponsorRows>
           </Stack>
           <Stack
-            direction="row" 
-            spacing={{ xs: 5, sm: 10, md: 15, lg: 20}}
+            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
+            spacing={10}
             justifyContent="center"
             alignItems="center"
+            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
           >
-            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', maxWidth: '400px' }}>
-              <img src="/sponsors/youngEngineersAustralia.jpg" alt="Young Engineers Australia" style={{ width: '100%', height: 'auto' }}/>
-            </Box>
+            <SponsorRows maxWidth='400px' imageComponent='/sponsors/youngEngineersAustralia.jpg' imageName='Young Engineers Australia'></SponsorRows>
           </Stack>
         </Stack>
       </Box>
