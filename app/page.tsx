ecdf66f38a2d67c4a722f0943685866acccf92a4
Sophia <z5312941@ad.unsw.edu.au>
2024-06-20 17:27:18 +1000

    mobile responsiveness but need to fixed centeredness

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {AspectRatio, Box, Stack, Typography} from '@mui/joy';
+import {AspectRatio, Box, Grid, Stack, Typography} from '@mui/joy';
 import Image from 'next/image';
 import LabelledIcon from '@/components/LabelledIcon';
 import EventsCarousel from '@/components/Carousel/EventsCarousel';
@@ -44,22 +44,29 @@ function AboutUsOverview() {
   We run many social and industry events throughout the year as well as two Programs dedicated to mentoring and interpersonal development.\n'
 
   return (
-    <Box mx='auto' my={15} width='1000px' height='100%'>
-      <Typography level='h1' textAlign={'left'}>About Us</Typography>
-      <Stack direction='row' spacing={15}>
-        <Box width='500px'>
-          <Typography level='body' pb={3}>{aboutUsText}</Typography>
-          <Stack direction='row' width='100%' height='100%' justifyContent='center' spacing={6}>
-            <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
-            <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
-            <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
-          </Stack>
+    <Box justifyContent='center'>
+      <Stack width={{xs: '90%', sm: '70%'}} mx='auto' my={15} spacing={5} direction={{md: 'column', lg: 'row'}} justifyContent='space-around' justifySelf='center'>
+        <Box justifyContent='center' mx='auto'>
+          <Typography level='h1' textAlign={'left'}>About Us</Typography>
+          <Box maxWidth='500px'>
+            <Typography level='body' pb={3}>{aboutUsText}</Typography>
+            <Stack direction='row' pb={5} justifyContent='center'>
+              <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
+              <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
+              <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
+            </Stack>
+          </Box>
         </Box>
-        <Box width='500px'>
-          <iframe width='100%' height='100%' src='https://www.youtube.com/embed/Tyyl9Kp-yiE?si=kk2dfErd_cJNYBbh'
-                  title='Welcome To WIESoc'
-                  allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                  referrerPolicy='strict-origin-when-cross-origin' allowFullScreen style={{border: 'none'}}></iframe>
+        <Box width='100%' maxWidth='500px' alignContent='center' justifyContent='center'>
+          <iframe
+            width='100%'
+            height='50%'
+            src='https://www.youtube.com/embed/Tyyl9Kp-yiE?si=kk2dfErd_cJNYBbh'
+            title='Welcome To WIESoc'
+            allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            referrerPolicy='strict-origin-when-cross-origin' allowFullScreen style={{border: 'none'}}
+          >
+          </iframe>
         </Box>
       </Stack>
     </Box>
