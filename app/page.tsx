462e59897ebeac00c5141e4bd399f8e249b4b72e
sophiac6410 <79835146+sophiac6410@users.noreply.github.com>
2024-06-22 12:59:31 +1000

    cleaned up responsiveness and code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {AspectRatio, Box, Grid, Stack, Typography} from '@mui/joy';
+import {AspectRatio, Box, Grid, Sheet, Stack, Typography} from '@mui/joy';
 import Image from 'next/image';
 import LabelledIcon from '@/components/LabelledIcon';
 import EventsCarousel from '@/components/Carousel/EventsCarousel';
@@ -40,36 +40,37 @@ function CoverImage() {
 }
 
 function AboutUsOverview() {
-  const aboutUsText = 'We are a UNSW student-run society that facilitates members from all disciplines of engineering. \n \
+  const aboutUsText = 'We are a UNSW student-run society that facilitates members from all disciplines of engineering. \n\n \
   We run many social and industry events throughout the year as well as two Programs dedicated to mentoring and interpersonal development.\n'
 
   return (
-    <Box justifyContent='center'>
-      <Stack width={{xs: '90%', sm: '70%'}} mx='auto' my={15} spacing={5} direction={{md: 'column', lg: 'row'}} justifyContent='space-around' justifySelf='center'>
-        <Box justifyContent='center' mx='auto'>
-          <Typography level='h1' textAlign={'left'}>About Us</Typography>
-          <Box maxWidth='500px'>
-            <Typography level='body' pb={3}>{aboutUsText}</Typography>
-            <Stack direction='row' pb={5} justifyContent='center'>
-              <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
-              <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
-              <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
-            </Stack>
-          </Box>
-        </Box>
-        <Box width='100%' maxWidth='500px' alignContent='center' justifyContent='center'>
-          <iframe
-            width='100%'
-            height='50%'
-            src='https://www.youtube.com/embed/Tyyl9Kp-yiE?si=kk2dfErd_cJNYBbh'
-            title='Welcome To WIESoc'
-            allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            referrerPolicy='strict-origin-when-cross-origin' allowFullScreen style={{border: 'none'}}
-          >
-          </iframe>
-        </Box>
+    <Stack
+      width={{md: '90%', lg: '70%', xl: '50%'}}
+      direction={{md: 'column', lg: 'row'}} 
+      mx='auto'
+      my={15}
+      spacing={6}
+      alignItems='center'
+    >
+      <Stack width={{xs: '90%', sm: '70%', md: '60%', lg: '50%'}} spacing={3} pb={3}>
+        <Typography level='h1' textAlign={'left'}>About Us</Typography>
+        <Typography level='body'>{aboutUsText}</Typography>
+        <Stack direction='row' justifyContent='center'>
+          <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
+          <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
+          <LabelledIcon icon='/landing-page/People.svg' text='2000+ members'/>
+        </Stack>
       </Stack>
-    </Box>
+      <AspectRatio sx={{width: {xs: '90%', sm: '70%', md: '60%', lg: '50%'}, alignContent: 'center'}}>
+        <iframe
+          src='https://www.youtube.com/embed/Tyyl9Kp-yiE?si=kk2dfErd_cJNYBbh'
+          title='Welcome To WIESoc'
+          allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          referrerPolicy='strict-origin-when-cross-origin' allowFullScreen style={{border: 'none'}}
+        >
+        </iframe>
+      </AspectRatio>
+    </Stack>
   );
 }
 
