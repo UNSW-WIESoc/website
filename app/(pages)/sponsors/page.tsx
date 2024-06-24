75f50b43b91a18a93350916eb0f253064cb8783b
unknown <aurelia.elaine@gmail.com>
2024-06-25 01:49:14 +1000

    adjusted image sizes and improved mobile responsiveness

--- app/(pages)/sponsors/page.tsx
+++ app/(pages)/sponsors/page.tsx
@@ -1,26 +1,64 @@
-import { Box, Stack, Typography } from '@mui/joy';
+import { AspectRatio, Box, Stack, Grid } from '@mui/joy';
 import PageHeaderCard from '@/components/PageHeaderCard';
 import HeadingBodyText from '@/components/HeadingBodyText';
 import SponsorSection from '@/components/SponsorSection';
 import Image from 'next/image';
 
-interface sponsorRows {
-  maxWidth: string;
-  imageComponent: string;
-  imageName: string;
+interface imageData {
+  src: string;
+  alt: string;
 }
 
-function SponsorRows ({maxWidth, imageComponent, imageName}: sponsorRows) {
+interface sponsorRows {
+  images: imageData[];
+}
+
+const premiumSponsors1 = [
+  { src: '/sponsors/atlassian.svg', alt: 'Atlassian' },
+  { src: '/sponsors/janeStreet_logo.jpg', alt: 'Jane Street' }
+];
+
+const premiumSponsors2 = [
+  { src: '/sponsors/sydneyWaters.jpg', alt: 'Sydney Waters' }
+];
+
+const advancedSponsors1 = [
+  { src: '/sponsors/cochlear.jpg', alt: 'Cochlear' },
+  { src: '/sponsors/aecom.jpg', alt: 'Aecom' },
+  { src: '/sponsors/dolby.jpg', alt: 'Dolby' },
+];
+
+const advancedSponsors2 = [
+  { src: '/sponsors/sportsGrid.jpg', alt: 'Sports Grid' },
+  { src: '/sponsors/systa.jpg', alt: 'Systra' }
+];
+
+const advancedSponsors3 = [
+  { src: '/sponsors/appian.svg', alt: 'Appian' },
+  { src: '/sponsors/projectServices.jpg', alt: 'Project Services' },
+  { src: '/sponsors/stantec.jpg', alt: 'Stantec' }
+];
+
+const foundationSponsors1 = [
+  { src: '/sponsors/honeywell.jpg', alt: 'Honeywell' },
+  { src: '/sponsors/flowTraders.jpg', alt: 'Flow Traders' }
+];
+
+const foundationSponsors2 = [
+  { src: '/sponsors/youngEngineersAustralia.jpg', alt: 'Young Engineers Australia' }
+];
+
+function SponsorRows ({ images }: sponsorRows) {
   return(
-    <Box
-      sx={{
-        position: 'relative',
-        width: '100%',
-        maxWidth: maxWidth
-      }}
-    >
-      <Image src={imageComponent} alt={imageName} layout='responsive' width={500} height={200}/>
-    </Box>
+    <Grid container spacing={10} justifyContent='center'>
+      {images.map((image, index) => (
+        <Grid xs={6} sm={5} md={4} lg={3} key={index}>
+          <AspectRatio variant='plain' objectFit='contain'>
+            <Image src={image.src} alt={image.alt} fill/>
+        </AspectRatio>
+      </Grid>
+      ))}
+    </Grid>
   )
 }
 
@@ -29,112 +67,28 @@ export default function Sponsors() {
     <Stack>
       <PageHeaderCard imagePath={'/sponsors/header.jpg'} pageTitle={'Sponsors'}/>
       <HeadingBodyText heading='Our Sponsors' body='WIESoc is proudly supported by the UNSW Faculty of Engineering and our industry partners.' color='graphite'/>
-      <Box 
-        display='flex' 
-        justifyContent='center' 
-        mt={2} 
-        mb={10}
-        sx={{
-          width: {xs: '440px', sm: '475px', md: '500px', lg: '525px'},
-          mx: 'auto',
-          height: 'auto'
-        }}  
-      >
-        <Box 
-          sx={{ 
-            position: 'relative',
-            width: '100%',
-            paddingBottom: '20%',
-            }}
-          >
-          <Image src="/sponsors/unsw_logo.jpg" alt="UNSW Engineering" fill/>
-        </Box>
-      </Box>
+      <Grid container justifyContent='center'>
+        <Grid xs={6} sm={5} md={4} lg={4}>  
+          <AspectRatio variant='plain' objectFit='contain'>
+            <Image src="/sponsors/unsw_logo.jpg" alt="UNSW Engineering" fill/>
+          </AspectRatio>
+        </Grid>
+      </Grid>
       <Box width='95%' mx='auto' mb={15}>
         <SponsorSection title='Premium Sponsors'/>
-        <Stack direction="column" spacing={8} justifyContent="center" alignItems="center">
-          <Stack 
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='400px' imageComponent='/sponsors/atlassian.svg' imageName='Atlassian'></SponsorRows>
-            <SponsorRows maxWidth='400px' imageComponent='/sponsors/janeStreet_logo.jpg' imageName='Jane Street'></SponsorRows>
-          </Stack>
-          <Stack
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='340px' imageComponent='/sponsors/sydneyWaters.jpg' imageName='Sydney Waters'></SponsorRows>
-          </Stack>
-        </Stack>
+        <SponsorRows images={premiumSponsors1}></SponsorRows>
+        <SponsorRows images={premiumSponsors2}></SponsorRows> 
       </Box>
       <Box width='95%' mx='auto' mb={15}>
         <SponsorSection title='Advanced Sponsors'/>
-        <Stack direction="column" spacing={8} justifyContent="center" alignItems="center">
-          <Stack 
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='320px' imageComponent='/sponsors/cochlear.jpg' imageName='Cochlear'></SponsorRows>
-            <SponsorRows maxWidth='340px' imageComponent='/sponsors/aecom.jpg' imageName='Aecom'></SponsorRows>
-            <SponsorRows maxWidth='340px' imageComponent='/sponsors/dolby.jpg' imageName='Dolby'></SponsorRows>
-
-          </Stack>
-          <Stack 
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='400px' imageComponent='/sponsors/sportsGrid.jpg' imageName='Sports Grid'></SponsorRows>
-            <SponsorRows maxWidth='450px' imageComponent='/sponsors/systa.jpg' imageName='Systra'></SponsorRows>
-          </Stack>
-          <Stack 
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='300px' imageComponent='/sponsors/appian.svg' imageName='Appian'></SponsorRows>
-            <SponsorRows maxWidth='400px' imageComponent='/sponsors/projectServices.jpg' imageName='Project Services'></SponsorRows>
-            <SponsorRows maxWidth='450px' imageComponent='/sponsors/stantec.jpg' imageName='Stantec'></SponsorRows>
-          </Stack>
-        </Stack>
+        <SponsorRows images={advancedSponsors1}></SponsorRows>
+        <SponsorRows images={advancedSponsors2}></SponsorRows>
+        <SponsorRows images={advancedSponsors3}></SponsorRows>
       </Box>
       <Box width='95%' mx='auto' mb={15}>
         <SponsorSection title='Foundation Sponsors'/>
-        <Stack direction="column" spacing={8} justifyContent="center" alignItems="center">
-          <Stack 
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='450px' imageComponent='/sponsors/honeywell.jpg' imageName='Honeywell'></SponsorRows>
-            <SponsorRows maxWidth='400px' imageComponent='/sponsors/flowTraders.jpg' imageName='Flow Traders'></SponsorRows>
-          </Stack>
-          <Stack
-            direction={{ xs: 'column', md: 'row', lg: 'row'}} 
-            spacing={10}
-            justifyContent="center"
-            alignItems="center"
-            sx= {{ width: { xs: '80%', sm: '65%', md: '85%', lg: '70%'} }}
-          >
-            <SponsorRows maxWidth='400px' imageComponent='/sponsors/youngEngineersAustralia.jpg' imageName='Young Engineers Australia'></SponsorRows>
-          </Stack>
-        </Stack>
+        <SponsorRows images={foundationSponsors1}></SponsorRows>
+        <SponsorRows images={foundationSponsors2}></SponsorRows>
       </Box>
     </Stack>
   );
