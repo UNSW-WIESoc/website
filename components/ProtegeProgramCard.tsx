792fef4840ab0f0cc40a1be018e7621fb58454ae
Aurelia <aurelia.elaine@gmail.com>
2024-07-19 17:01:22 +1000

    Added protege program card component

---
+++ components/ProtegeProgramCard.tsx
@@ -0,0 +1,13 @@
+import {AspectRatio, Box, Stack, Typography} from '@mui/joy';
+import LabelledIcon from '@/components/LabelledIcon';
+import OutlineButton from '@/components/OutlineButton';
+
+export default function ProtegeProgramCard({}) {
+  return (
+    <Box>
+      <Typography level='subtitle-lg'>
+        Want to get invovled in the Protege program?
+      </Typography>
+    </Box>
+  )
+}
