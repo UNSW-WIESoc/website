eb6477957383482e9e7c2dce19e0febe58b8523c
Rosa <r.pak@student.unsw.edu.au>
2024-07-25 14:51:23 +1000

    fixed message box input location

--- app/(pages)/contact-us/page.tsx
+++ app/(pages)/contact-us/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, FormControl, Input, Button } from '@mui/joy';
+import { Box, Stack, FormControl, Input, Button, Textarea } from '@mui/joy';
 import PageHeaderCard from '@/components/PageHeaderCard';
 import HeadingBodyText from '@/components/HeadingBodyText';
 
@@ -9,25 +9,28 @@ export default function ContactUs() {
     <Stack>
       <PageHeaderCard imagePath={'/contact-us/header.jpg'} pageTitle={'Contact Us'}/>
       <HeadingBodyText heading='Contact Us' body={bodyText} color='graphite'/>
-      <Stack spacing={3.5} pb={4} alignItems='center'>
+      <Stack spacing={5} pb={4} alignItems='center'>
         <Box display='flex' justifyContent='center' sx={{width:'70%', height:'4em', gap:6}}>
-          {/* <FormControl > */}
-            <Input className='medium-blue' placeholder='First Name' variant='outlined' sx={{color:'blue', width:'100%'}}/>
-            <Input className='medium-blue' placeholder='Last Name' variant='outlined' sx={{color:'blue', width:'100%'}}/>
-          {/* </FormControl> */}
+            <Input className='medium-blue' placeholder='First Name' variant='outlined' sx={{color:'blue', width:'100%', paddingLeft: '4%', fontSize:'0.9em'}}/>
+            <Input className='medium-blue' placeholder='Last Name' variant='outlined' sx={{color:'blue', width:'100%', paddingLeft: '4%', fontSize:'0.9em'}}/>
         </Box>
         <Box display='flex' justifyContent='center' sx={{width:'70%', height:'4em', gap:6}}>
-          {/* <FormControl> */}
-            <Input className='medium-blue' placeholder='Email Address' variant='outlined' sx={{color:'medium-blue', width:'100%'}}/>
-            <Input className='medium-blue' placeholder='Contact Number' variant='outlined' sx={{width:'100%'}}/>
-          {/* </FormControl> */}
+            <Input className='medium-blue' placeholder='Email Address' variant='outlined' sx={{color:'medium-blue', width:'100%', paddingLeft: '4%', fontSize:'0.9em'}}/>
+            <Input className='medium-blue' placeholder='Contact Number' variant='outlined' sx={{width:'100%', paddingLeft: '4%', fontSize:'0.9em'}}/>
         </Box>
-        <Box justifyContent='center' sx={{width:'70%'}}>
-          <FormControl>
-            <Input className='medium-blue' placeholder='Message' variant='outlined' sx={{width:'100%', color:'blue', height:'17em', verticalAlign:'top'}}/>
-          </FormControl>
-        </Box>
-        <Button className='medium-blue' variant="outlined" sx={{width:'13%'}}>Send Message</Button>        
+        <Textarea
+          className='medium-blue'
+          placeholder="Message"
+          minRows={15}
+          style={{
+            width: '70%',
+            resize: 'vertical',
+            paddingTop: '2.5%', 
+            paddingLeft: '3%',
+            fontSize:'0.9em'
+          }}
+        />
+        <Button className='medium-blue' variant="outlined" sx={{width:'19%', fontSize: '1em', fontWeight:'bold', padding:'1% 0', borderRadius:'20px', borderWidth:'2px'}}>Send Message</Button>        
       </Stack>
     </Stack>
   );
