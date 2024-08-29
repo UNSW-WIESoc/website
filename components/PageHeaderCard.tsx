64e33f4aef290e688398f85e7d4cbcaf1801d609
Arundhathi Madhu <z5420435@ad.unsw.edu.au>
2024-08-29 17:26:37 +1000

    changing back to original

--- components/PageHeaderCard.tsx
+++ components/PageHeaderCard.tsx
@@ -9,7 +9,7 @@ interface PageHeaderCardProps {
 export default function PageHeaderCard({imagePath, pageTitle} : PageHeaderCardProps) {
   return (
       <AspectRatio ratio='700/150' objectFit='cover'
-                   sx={{width: '100%', height:'auto', position: 'relative', overflow:'hidden'}}>
+                   sx={{width: '100%', height: '300px', position: 'relative', overflow:'hidden'}}>
         <Image src={imagePath} alt='page-header-image' fill/>
         <Box
           className='bg-dark-blue'
@@ -18,7 +18,7 @@ export default function PageHeaderCard({imagePath, pageTitle} : PageHeaderCardPr
             top: 0,
             left: 0,
             width: '100%',
-            height: '100%',
+            height: '300px',
             opacity: '0.6',
           }}
         />
@@ -28,7 +28,7 @@ export default function PageHeaderCard({imagePath, pageTitle} : PageHeaderCardPr
             top: 0,
             left: 0,
             width: '100%',
-            height: '100%',
+            height: '300px',
           }}
           alignItems='center'
           justifyContent='center'
