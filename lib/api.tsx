98941821c1479acaa007c2df5f31f86eb87d5751
Rebecca Hsu <rbeccahsu@gmail.com>
2024-08-29 16:59:59 +0900

    Setup team contentful and endpoints

--- lib/api.tsx
+++ lib/api.tsx
@@ -6,6 +6,7 @@ const client = createClient({
   accessToken: `${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
 })
 
+// Gets all events data
 export async function getEvents() {
   const res = await client
     .getEntries({
@@ -15,4 +16,28 @@ export async function getEvents() {
       console.error(error);
     });
   return res.items;
+}
+
+// Gets all exec team data
+export async function getExecs() {
+  const res = await client
+    .getEntires({
+      content_type: 'execs'
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+    return res.items;
+}
+
+// Gets all portfolio (director and subcommittee) data
+export async function getPortfolio() {
+  const res = await client
+    .getEntires({
+      content_type: 'execs'
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+    return res.items;
 }
