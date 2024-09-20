45db3afe52fbf7d794d51a028a3454be3226e44b
DaisyYye <daisydaisyye@gmail.com>
2024-09-20 17:08:22 +1000

    simplified code

--- components/ProgramsTimeline.tsx
+++ components/ProgramsTimeline.tsx
@@ -2,78 +2,10 @@ import { Box, Grid } from '@mui/material';
 import Image from 'next/image';
 import { FC } from 'react';
 import Typography from '@mui/joy/Typography';
-interface TimelineCardProps {
-  icon: string;
-  isLast: boolean;
-}
-
 interface TextComponentProps {
   text: string;
   date: string;
 }
-
-const Arrow = () => {
-  return (
-    <Box
-    sx={{
-      display: 'flex',
-      flexDirection: { xs: 'column', sm: 'column', md: 'row' }, 
-      alignItems: 'center',
-      position: 'relative',
-      marginTop: '10px',
-      '&:before': {
-        content: '""',
-        display: 'block',
-        width: { xs: '2px', sm: '2px', md: '80px' }, 
-        height: { xs: '25px', sm: '25px', md: '0px' }, 
-        borderBottom: { xs: 'none', sm: 'none', md: '2px dashed white'},
-        borderLeft: { xs: '2px dashed white', sm: '2px dashed white', md: '0px' },
-        position: 'relative',
-        marginLeft: { xs: '0px', sm: '0px', md: '275px' },
-        top: { xs: '0px', sm: '0px', md: '-65px' },
-      },
-      '&:after': {
-        content: '""',
-        display: 'block',
-        borderTop: { xs: '4px solid white', sm: '4px solid white', md: '4px solid transparent' },
-        borderBottom: { xs: 'none', sm: 'none', md: '4px solid transparent' },
-        borderLeft: { xs: '4px solid transparent', sm: '4px solid transparent', md: '4px solid white' },
-        borderRight: { xs: '4px solid transparent', sm: '4px solid transparent', md: 'none' },
-        transform: { xs: 'translateY(0)', sm: 'translateY(0)', md: 'translateY(-65px)' },
-      },
-    }}
-  />
-  );
-};
-
-const TimelineCard: FC<TimelineCardProps> = ({ icon, isLast }) => (
-  <Box
-    textAlign="center"
-    sx={{
-      display: 'flex',
-      flexDirection: { xs: 'column', sm: 'column', md: 'column' }, 
-      alignItems: { xs:'center', sm: 'center', md: 'center' },
-      justifyContent: 'center',
-      width: '100%',
-    }}
-  >
-    <Box
-      sx={{
-        width: { xs: '90px', sm: '90px', md: '110px' }, 
-        height: { xs: '90px', sm: '90px', md: '110px' },
-        borderRadius: '50%',
-        border: '1px solid white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        aspectRatio: '1'
-      }}
-    >
-      <Image src={icon} alt={''} width={60} height={60} />
-    </Box>
-    {!isLast && <Arrow />}
-  </Box>
-);
 
 const TextComponent: FC<TextComponentProps> = ({ text, date }) => (
   <Box
@@ -101,21 +33,11 @@ const TextComponent: FC<TextComponentProps> = ({ text, date }) => (
   </Box>
 );
 
-const Mobileitems = [
-  { component: <TimelineCard icon='/programs/Stage.svg' isLast={false} key="1" />, xs: 8, sm: 8 },
-  { component: <TextComponent text='Introductory Ceremony' date='7th March 2024' key="2" />, xs: 4, sm: 4 },
-  { component: <TimelineCard icon='/programs/Level1.svg' isLast={false} key="3" />, xs: 8, sm: 8 },
-  { component: <TextComponent text='Mid-term Checkpoint 1' date='14th March 2024' key="4" />, xs: 4, sm: 4 },
-  { component: <TimelineCard icon='/programs/Level2.svg' isLast={false} key="5" />, xs: 8, sm: 8 },
-  { component: <TextComponent text='Mid-term Checkpoint 2' date='23rd March 2024' key="6" />, xs: 4, sm: 4 },
-  { component: <TimelineCard icon='/programs/Trophy.svg' isLast={true} key="7" />, xs: 8, sm: 8 },
-  { component: <TextComponent text='Concluding Ceremony ' date='11th April 2024' key="8" />, xs: 4, sm: 4 }
-];
 
 const Timeline = () => {
   return (
     <Box className='bg-medium-blue' sx={{ justifyContent: 'center', pt: 8, pb: 8 }} mb={10} width='100%'>
-      <Typography level='subtitle-lg' className='light-white' sx={{ mb: 4, textAlign: 'center'}}>
+      <Typography level='subtitle-lg' className='light-white' sx={{ mb: 3, textAlign: 'center'}}>
       Checkpoint Events
       </Typography>
 
@@ -123,28 +45,32 @@ const Timeline = () => {
         <Box sx={{ display: { xs: 'none', md: 'block' }, flexGrow: 1 }}>
           <Box 
             sx={{ display: 'flex', justifyContent: 'center', mb: -52, mt: -50, mr: -1.5 }}>
-            <Image src='/programs/Timeline.svg' alt='Timeline' width={961} height={961} style={{ display: 'block' }} />
+            <Image src='/programs/Timeline.svg' alt='Timeline' width={950} height={950} style={{ display: 'block' }} />
           </Box>
           <Box
-            sx={{display: 'flex', flexDirection: { md: 'row' },  justifyContent: 'center', gap: '130px',}}>
-            <TextComponent text='Introductory Ceremony' date='7th March 2024' />
+            sx={{display: 'flex', flexDirection: { md: 'row' },  justifyContent: 'center', mr: -2, gap: '132px',}}>
+            <TextComponent text='Opening Ceremony' date='7th March 2024' />
             <TextComponent text='Mid-term Checkpoint 1' date='14th March 2024' />
             <TextComponent text='Mid-term Checkpoint 2' date='23rd March 2024' />
             <TextComponent text='Concluding Ceremony ' date='11th April 2024' />
           </Box>
         </Box>
 
-        <Box sx={{ display: { xs: 'block', sm: 'block', md: 'none' }, flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            {Mobileitems.map((item, index) => (
-              <Grid item xs={item.xs} sm={item.sm} key={index}>
-                {item.component}
-              </Grid>
-            ))}
-          </Grid>
+        <Box sx={{ display: { xs: 'block', sm: 'block', md: 'none' },  flexDirection: { xs: 'column', sm: 'column'}, alignItems: 'center', flexGrow: 1 }}>
+          <Box 
+            sx={{ display: 'flex', justifyContent: { xs: 'center', md: 'flex-start' }, mb: 0, mt: 0, mr: 30}}>
+            <Image src='/programs/Verticaltimeline.svg' alt='Timeline' width={510} height={510} style={{ display: 'block' }} />
+          </Box>
+          <Box
+            sx={{ display: 'flex', flexDirection: {xs: 'column',  sm: 'column'}, mt: -65.5, mr: -30, justifyContent: 'center', gap: '63px'}}>
+            <TextComponent text='Opening Ceremony' date='7th March 2024' />
+            <TextComponent text='Mid-term Checkpoint 1' date='14th March 2024' />
+            <TextComponent text='Mid-term Checkpoint 2' date='23rd March 2024' />
+            <TextComponent text='Closing Ceremony ' date='11th April 2024' />
+          </Box>
+        </Box>
         </Box>
       </Box>
-    </Box>
   )
 }
 
