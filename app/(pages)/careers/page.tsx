2f2c11fef0578870ed89c2fbf0b9704294dce456
Noor <z5417124@ad.unsw.edu.au>
2024-10-13 20:44:29 +1100

    Added the required fields on contentful and added the EventsCard layout for the Careers page

--- app/(pages)/careers/page.tsx
+++ app/(pages)/careers/page.tsx
@@ -5,7 +5,7 @@ import { Grid, Stack, Box } from '@mui/joy';
 import PageHeaderCard from "@/components/PageHeaderCard";
 import HeadingBodyText from '@/components/HeadingBodyText';
 import { getJobs } from "@/lib/api";
-import EventsCarousel from '@/components/Carousel/EventsCarousel';
+import EventsCard from '@/components/EventsCard';
 
 export default function Careers() {
   const [currJobs, setCurrJobs] = useState([]);
@@ -13,7 +13,8 @@ export default function Careers() {
   useEffect(() => {
     async function fetchData() {
       const jobs = await getJobs();
-      setCurrJobs(jobs);
+      const now = new Date();
+      setCurrJobs(jobs.filter((job: any) => new Date(job.fields.dateTimeEnd) > now));
     }
 
     fetchData();
@@ -21,9 +22,33 @@ export default function Careers() {
 
    return (
     <Stack alignItems='center'>
-    <PageHeaderCard imagePath={'/careers/header.jpg'} pageTitle={'Careers'}/>
-    <HeadingBodyText heading='Jobs and Opportunities' body='Discover corporate 
-      sponsored events and activities to expand your professional networks and skills.' color='graphite' />
+      <PageHeaderCard imagePath={'/careers/header.jpg'} pageTitle={'Careers'}/>
+      <HeadingBodyText heading='Jobs and Opportunities' body='Discover corporate 
+        sponsored events and activities to expand your professional networks and skills.' color='graphite' />
+      <Grid
+        container
+        justifyContent='center'
+        sx={{ 'width': {xs: '100%', sm: '90%', md: '80%', lg: '70%', xl: '60%'} }}
+        spacing={10}
+        mb={10}
+      >
+        {currJobs && currJobs?.map((event: any, index: number) => (
+          <Grid
+            xs={12} sm={12} md={6}
+            key={index}
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+          >
+            <Box flexGrow={1} justifyContent='center'>
+              <EventsCard
+                key={index}
+                event={event}
+              />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </Stack>
   );
 }
