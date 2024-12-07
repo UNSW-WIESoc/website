2ead701d3808b804f4564541d2493013afeb9f0c
Rebecca Hsu <rbeccahsu@gmail.com>
2024-12-07 13:05:59 +1100

    Update footer email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
           </Stack>
           <Stack direction='column' spacing={2} py={7}>
             <Typography level='body'>Have more questions? Contact us here:</Typography>
-            <Typography level='body'>admin@wiesoc.com.au</Typography>
+            <Typography level='body'>admin@wiesoc.com</Typography>
           </Stack>
         </center>
       </Box>
