ea1feab5453bbede8dfbdb5868181e3aeab939cc
noor367 <nh@lappy.local>
2025-03-23 01:40:24 +1100

    Edit the object fields to just get the info we want

--- app/(pages)/newsletter/page.tsx
+++ app/(pages)/newsletter/page.tsx
@@ -1,47 +1,48 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
-import {Grid, Stack, Box, Typography} from '@mui/joy';
+import {Stack} from '@mui/joy';
 import PageHeaderCard from '@/components/PageHeaderCard';
-import HeadingBodyText from '@/components/HeadingBodyText';
-import EventsCarousel from '@/components/Carousel/EventsCarousel';
-import { getEvents } from "@/lib/api";
+import { getNewsletter } from '@/lib/api';
+import Carousel from '@/components/Carousel/Carousel';
 
 export default function Newsletter() {
-  const [pastEvents, setPastEvents] = useState([]);
-
+  const [pastNews, setPastNews] = useState([]);
+  const OPTIONS = {
+    loop: true, 
+    skipSnaps: true,
+    align: 'center',
+    slidesToScroll: 'large'
+  }
+  
   useEffect(() => {
     async function fetchData() {
-      const events = await getEvents();
-
-      const now = new Date();
-
-      const upcoming = events.filter((event: any) => new Date(event.fields.dateTimeEnd) > now);
-      const past = events.filter((event: any) => new Date(event.fields.dateTimeEnd) <= now);
-
-      setPastEvents(past);
+      const items = await getNewsletter();
+      console.log("Fetched Newsletters:", items);
+  
+      const slides = items.map((item: any) => ({
+        title: item.fields.title,
+        date: item.fields.date,
+        pdfUrl: item.fields.file.fields.url
+      }));
+      console.log("Slides:", slides);
+  
+      setPastNews(slides);
     }
-
     fetchData();
-  }, []);
-
-  const example = {
-    closeDate: "",
-    img: "",
-    position: "",
-    location: "",
-    summary: "",
-    companyName: "",
-    link: "/public/"
-  }
+  }, []);  
 
   return (
     <Stack alignItems='center'>
         {/* TODO: CHANGE IMAGE */}
       <PageHeaderCard imagePath={'/events/header.jpg'} pageTitle={'Newsletter'}/> 
-      <HeadingBodyText heading="WIESoc O'Clock" body="Keep up to date on event highlights and upcoming opportunities with our new monthly newsletter!" color='graphite'/>
       {/* Add a current pub card? */}
-      <EventsCarousel heading='Archive' body='Browse through our previous publications!' slides={pastEvents} size='small'/>
+      <Carousel 
+        slides={pastNews} 
+        size='small'
+        options={OPTIONS}
+        type='newsletter'
+      />
     </Stack>
   );
 };
